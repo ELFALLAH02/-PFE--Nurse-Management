d552e3296acb63086a34e73649183fa2011c5079
ELBALGHY <98643819+Elbalghy@users.noreply.github.com>
2022-05-15 03:45:41 +0100

    Create Class the Varibles Global

---
+++ (PFE)-Nurse-Management/ClassVrariables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace _PFE__Nurse_Management
+{
+   public class ClassVrariables
+    {
+        public static SqlConnection ctn = new SqlConnection("Data Source=.;Initial Catalog=Gestion_de_infirmier;Integrated Security=True");
+        public static SqlCommand cmd;
+        public static SqlDataAdapter da;
+    }
+}
